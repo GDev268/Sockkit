f70690522a5d893f89d96b58d208f954af8e2995
GDev268 <gcgranja@gmail.com>
2025-09-07 00:40:52 +0100

    Visibility Updates Pt.3

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,31 @@
+use std::fmt;
+
 #[derive(Debug)]
-pub(crate) enum ReadError {
+pub enum ReadError {
     EmptyBuffer,
     FailedToRead(std::io::Error)
 }
 
 #[derive(Debug)]
-pub(crate) enum WriteError {
+pub enum WriteError {
     EmptyBuffer,
     FailedToWrite(std::io::Error)
+}
+
+impl fmt::Display for ReadError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ReadError::EmptyBuffer => write!(f, "read error: buffer is empty"),
+            ReadError::FailedToRead(err) => write!(f, "read error: failed to read: {}", err),
+        }
+    }
+}
+
+impl fmt::Display for WriteError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            WriteError::EmptyBuffer => write!(f, "write error: buffer is empty"),
+            WriteError::FailedToWrite(err) => write!(f, "write error: failed to write: {}", err),
+        }
+    }
 }
